6b70cb42f086abfa4c3b2651d86ac81212455b77
brandonophus <brandonophus@gmail.com>
2017-03-20 16:21:43 -0700

    final styling edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('#buttonStart').click( function() {
   $('#startScreen').fadeout();
 });
+
+$(document).ready( function(){
+  $('#beerLager').addClass('active');
+});
+
+function changeBeer(beertype) {
+  $('.active').removeClass('active');
+  $('#' + beertype + ', #beer' + beertype).addClass('active');
+}
